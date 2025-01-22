8727f7b524dbc2d70c4b428d3442ef88a5fc903e
Leo Gong <leogong99@gmail.com>
2025-01-21 17:59:02 -0800

    Add logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import Home from './components/Home/Home';
 import Chatbot from './components/Chatbot/Chatbot';
 import './App.css';
 
+import logo from './images/logo.png';
+
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1>Math Buddy</h1>
+          <div className="app-header-title">
+            <img src={logo} alt="logo" className='logo'/>
+            <h1>Math Buddy</h1>
+          </div>
           <nav className="nav-links">
             <Link to="/" className="nav-link">Home</Link>
             <Link to="/chat" className="nav-link">Chat</Link>
